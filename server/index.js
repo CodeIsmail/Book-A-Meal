8d81faa47694ffc72a5e6f325a079c319263cf0b
Ismail <codeismail@gmail.com>
2018-04-25 02:35:55 +0100

    #157048157 server configured

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    res.send("hello worlld");
+  res.send('Congratulations, You\'ve set up your server');
 });
 
-app.listen(3000, () =>{
-    console.log("Server started at port 3000");
+app.listen(3000, () => {
+  console.log('Server started at port 3000');
 });
