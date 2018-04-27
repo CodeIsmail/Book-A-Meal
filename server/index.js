88f7ecf601a811a7b6cf5129a7bd68b8be35f465
Ismail <codeismail@gmail.com>
2018-04-27 20:46:50 +0100

    #157048157 change port environment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
@@ -9,6 +10,6 @@ app.get('/', (req, res) => {
   res.send('Congratulations, You\'ve set up your server');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server started at port 3000');
 });
