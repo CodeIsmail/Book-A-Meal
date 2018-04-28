d86b3bf577414ff1497bea528990f9067a70f461
Ismail <codeismail@gmail.com>
2018-04-28 12:44:18 +0100

    #157168006 add meal route

--- server/routes/api/v1.js
+++ server/routes/api/v1.js
@@ -4,7 +4,7 @@ import mealsRoutesIndex from '../api/v1/index';
 const router = express.Router();
 
 router.use('/v1', mealsRoutesIndex);
-router.use('/v1', (req, res) => {
+router.get('/v1', (req, res) => {
   res.send('Welcome!');
 });
 
