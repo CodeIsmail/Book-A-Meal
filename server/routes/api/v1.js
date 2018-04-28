72307cead4f102036b83538242de94668bd72b60
Ismail <codeismail@gmail.com>
2018-04-28 03:53:29 +0100

    #156972107 utility route added

--- server/routes/api/v1.js
+++ server/routes/api/v1.js
@@ -4,5 +4,8 @@ import mealsRoutesIndex from '../api/v1/index';
 const router = express.Router();
 
 router.use('/v1', mealsRoutesIndex);
+router.use('/v1', (req, res) => {
+  res.send('Welcome!');
+});
 
 module.exports = router;
